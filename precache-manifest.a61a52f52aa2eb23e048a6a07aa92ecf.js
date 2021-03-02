a07974d1bc788ef0f17def9e2d2b7763d141e3a9
ElizarovArtem <elizarovskiyklach@yandex.ru>
2021-03-03 01:49:38 +0300

    Updates

---
+++ precache-manifest.a61a52f52aa2eb23e048a6a07aa92ecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6358c2935140e80941c17ec0414b5e29",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "2b8e8f42a5a18ce94a4f",
+    "url": "/TodoList/static/css/main.a5398f7b.chunk.css"
+  },
+  {
+    "revision": "7ccd32a939b424b52fca",
+    "url": "/TodoList/static/js/2.fd426967.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/TodoList/static/js/2.fd426967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8e8f42a5a18ce94a4f",
+    "url": "/TodoList/static/js/main.3acedcf8.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
